819bf1781151b89174e5a89b54403cd48b0b33ae
keanna21 <keannathompson@yahoo.com>
2023-04-12 17:23:56 -0500

    add details

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Bird
+
+
 def home(request):
     return HttpResponse('<h1>Hello World!</h1>')
 
@@ -10,19 +13,10 @@ def about(request):
     return render(request, 'about.html')
 
 def birds_index(request):
+    birds = Bird.objects.all()
      
     return render(request, 'birds/index.html', {'birds': birds})
 
-
-class Bird:  
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-birds = [
-  Bird('Screech', 'cardinal', 'red in color', 7),
-  Bird('Zippy', 'hummingbird', 'long narrow bill', 0),
-  Bird('Scruffy', 'Owl', 'night watcher', 3)
-]
+def birds_detail(request, bird_id):
+    bird = Bird.objects.get(id=bird_id)
+    return render(request, 'birds/detail.html', {'bird': bird})
